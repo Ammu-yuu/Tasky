9937a1aa52f2c3f97a3342f86adeb0730f4a982d
Anya <anya_armr@outlook.com>
2025-06-16 02:24:42 +0100

    home background

---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="bottom-full flex items-end justify-center pb-10">
+            <button className="px-6 py-3 bg-pink-100 text-gray-800 font-bold border border-pink-300 shadow-md hover:bg-pink-200 transition rounded">
+                Press to Start
+            </button>
+        </div>
+    );
+}
